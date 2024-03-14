94d8117fc44bc391e6ebb9954f7c414d61cf5ee4
Francis Lam <flam@alum.mit.edu>
2024-03-14 21:05:00 +0000

    search: add logging and what not

--- tinygrad/features/search.py
+++ tinygrad/features/search.py
@@ -36,15 +36,22 @@ def _time_program(variables:List[Variable], rdev:Compiled, lib:bytes, global_siz
   factor = 1
   if global_size is not None and max_global_size is not None:
     global_size, factor = _get_test_global_size(global_size, max_global_size, var_vals)
+  if BEAM_LOG: print(f"_time_program: before car")
   try: car = CompiledASTRunner(name, "", rdev, global_size, local_size, variables=variables, precompiled=lib)
   except AssertionError: return [math.inf] * cnt
   tms = []
   for _ in range(cnt):
+    if BEAM_LOG: print(f"_time_program: before rand")
     if clear_l2:
       if hasattr(rdev, 'inv_cache'): rdev.inv_cache()
       else:
         with Context(DEBUG=0): Tensor.rand(1024,1024).realize()
-    tms.append(cast(float, car(rawbufs, var_vals, wait=True, do_update_stats=False))*factor)
+    if BEAM_LOG: print(f"_time_program: before car")
+    try: tms.append(cast(float, car(rawbufs, var_vals, wait=True, do_update_stats=False))*factor)
+    except BaseException as e:
+      print(f"_time_program error {e}")
+      return [math.inf] * cnt
+    if BEAM_LOG: print(f"_time_program: after car")
     if early_stop is not None and early_stop < tms[-1]: break
   return tms
 
@@ -57,7 +64,12 @@ def _compile_linearizer(compiler:Compiler, lin:Linearizer, name:Optional[str]=No
 
 def _try_compile_linearized_w_idx(x, compiler:Compiler):
   try: return (x[0], _compile_linearizer(compiler, x[1], "test"))
-  except Exception:
+  except KeyboardInterrupt: raise
+  except BaseException as e:
+    print(f"search._compile_linearizer compile exception: {e}")
+    print(f"ast={x[1].ast}")
+    print(f"applied_opts={x[1].applied_opts}")
+    print(x[1].colored_shape())
     if DEBUG >= 4: traceback.print_exc()
     return (x[0], None)
 
@@ -93,11 +105,11 @@ def get_linearizer_actions(lin:Linearizer, include_0=True) -> Dict[int, Lineariz
         if c in {"cyan", "green", "white"}: lcl *= s
       if up > max_up or lcl > max_lcl: continue
       acted_lins[i+1] = lin2
-    except KernelOptError:
-      pass
+    except KernelOptError: pass
   return acted_lins
 
 beam_pool = None
+BEAM_LOG = getenv("BEAM_LOG", 0)
 def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linearizer:
   global beam_pool
   key = {"ast": lin.ast.key, "amt": amt, "allow_test_size": allow_test_size, "device": lin.opts.device, "suffix": lin.opts.suffix}
@@ -110,7 +122,9 @@ def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linea
   seen_libs = set()
 
   default_parallel, min_progress_micros = 1 if lin.opts.device in {"CUDA", "HIP", "HSA"} else 0, getenv("BEAM_MIN_PROGRESS",0.01)
-  if beam_pool is None and getenv("PARALLEL", default_parallel): beam_pool = multiprocessing.Pool(multiprocessing.cpu_count(), _init_worker)
+  if beam_pool is None and getenv("PARALLEL", default_parallel):
+    ctx = multiprocessing.get_context("spawn")
+    beam_pool = ctx.Pool(multiprocessing.cpu_count(), _init_worker, (), None)
 
   try:
     var_vals = {k:(k.max+k.min)//2 for k in lin.ast.vars()}
@@ -127,11 +141,20 @@ def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linea
         if lib in seen_libs: continue
         #print(acted_lins[i].colored_shape(), acted_lins[i].applied_opts)  # for debugging BEAMs that segfault
         seen_libs.add(lib)
+        if BEAM_LOG: print(f"{i=}: ", end="")
         try: tms = _time_program(vars, dev, lib, global_size, local_size, var_vals, rawbufs,
                                  early_stop=beam[0][1]*3 if len(beam) else 1.0, clear_l2=hasattr(dev, 'inv_cache'))
         except RuntimeError: continue # for runtime issues
+        except KeyboardInterrupt: raise
+        except BaseException as e:
+          print(f"beam_search: failed to _time_program: {e}")
+          print(acted_lins[i].ast)
+          print(acted_lins[i].applied_opts)
+          print(acted_lins[i].colored_shape())
+          continue
         timed_lins.append((acted_lins[i], min(tms)))
-        if DEBUG >= 2: print(f"\r{time.perf_counter() - st:7.2f}s: {timed_lins[-1][1]*1e6:12.2f} us       {len(timed_lins):4d}/{len(acted_lins):4d}         {timed_lins[-1][0].colored_shape()}\033[K", end="")  # noqa: E501
+        if BEAM_LOG: print(f"{time.perf_counter() - st:7.2f}s: {timed_lins[-1][1]*1e6:12.2f} us       {len(timed_lins):4d}/{len(acted_lins):4d}         {timed_lins[-1][0].colored_shape()}")  # noqa: E501
+        elif DEBUG >= 2: print(f"\r{time.perf_counter() - st:7.2f}s: {timed_lins[-1][1]*1e6:12.2f} us       {len(timed_lins):4d}/{len(acted_lins):4d}         {timed_lins[-1][0].colored_shape()}\033[K", end="")  # noqa: E501
 
       # done
       opts = sorted(timed_lins, key=lambda x: x[1])
@@ -144,6 +167,12 @@ def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linea
     if beam_pool is not None: beam_pool.terminate()
     raise e
 
+  if beam_pool is not None and False:
+    print(f"beam_pool before terminate")
+    beam_pool.terminate()
+    print(f"beam_pool before join")
+    beam_pool.join()
+    print(f"beam_pool after join")
   if CACHELEVEL >= 1: diskcache_put("beam_search", key, beam[0][0].applied_opts)
   if DEBUG >= 3: print(beam[0][0].applied_opts)
   return beam[0][0]
@@ -168,8 +197,11 @@ def time_linearizer(lin:Linearizer, rawbufs:List[Buffer], allow_test_size=True, 
   assert isinstance(dev, Compiled) and dev.compiler is not None
 
   var_vals = {k:(k.max+k.min)//2 for k in lin.ast.vars()}
+  if BEAM_LOG: print(f"time_linearizer._compile_linearizer")
   lib, global_size, local_size, vars = _compile_linearizer(dev.compiler, lin)
+  if BEAM_LOG: print(f"time_linearizer._time_program")
   tms = _time_program(vars, dev, lib, global_size, local_size, var_vals, rawbufs, max_global_size=max_global_size if allow_test_size else None, clear_l2=clear_l2, cnt=cnt, name=to_function_name(lin.name))  # noqa: E501
+  if BEAM_LOG: print(f"time_linearizer._time_program: {min(tms)}")
 
   if CACHELEVEL >= 2: diskcache_put("time_linearizer", key, tms)
   return min(tms)
