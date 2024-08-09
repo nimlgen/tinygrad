6bad798057831d76e8eeb125e7e2efef0ed7e97c
Comma Device <device@comma.ai>
2024-08-09 13:38:18 -0700

    working DSP backend

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -5,8 +5,9 @@ from tinygrad.helpers import cpu_time_execution, DEBUG, cpu_objdump
 from tinygrad.renderer.cstyle import ClangRenderer
 
 class ClangCompiler(Compiler):
-  def __init__(self, args:Optional[List[str]]=None):
+  def __init__(self, cachekey, args:Optional[List[str]]=None):
     self.args = ['-march=native'] if args is None else args
+    super().__init__(cachekey)
 
   def compile(self, src:str) -> bytes:
     # TODO: remove file write. sadly clang doesn't like the use of /dev/stdout here
